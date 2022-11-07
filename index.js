1f5572d3b81208bd5097b0a8c9b951f37b3702b8
Mato <Kittawat@icloud.com>
2022-11-07 23:02:46 +0800

    created logic

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-alert("testing");
+function plus(a, b) {
+  return a + b;
+}
+ 
+function minus(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return plus(a, b);
+        case '-':
+            return minus(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
